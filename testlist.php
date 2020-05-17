e933c739e987d46c60a48c466b4d4e28f8937aa4
haideralwaqfi <haideralwaqfi@gmail.com>
2020-05-17 22:21:26 +0300

    Version 1.3

--- testlist.php
+++ testlist.php
@@ -13,7 +13,7 @@ require '../h2m/html/testlist.html';
     if($result->num_rows > 0){
         while($row = $result->fetch_assoc()){
             echo "<tr><th class='fixed-side'>".$row['test_name']."</th><td>"
-            .$row['sample_type']."</td><td>"
+            .$row['sample_type']."</td><td class='ref_low'>"
             .$row['ref_low']."</td><td>"
             .$row['ref_high']."</td><td>
             <button type='button' name='enter' id='enter' data-toggle='modal' data-target='#add_data_Modal' class='enter btn btn-warning'>Edit</button><button class='btn btn btn-info btn-copy'>Copy</button></td><td class='ref_low_m'>"
@@ -62,14 +62,14 @@ if(isset($_POST['searchall'])){
     if($result->num_rows > 0){
       while($row = $result->fetch_assoc()){
           echo "<tr><th class='fixed-side'>".$row['test_name']."</th><td>"
-                         .$row['sample_type']."</td><td>"
-                         .$row['ref_low']."</td><td>"
+                         .$row['sample_type']."</td><td class='ref_low'>"
+                         .$row['ref_low']."</td><td class='ref_high'>"
                          .$row['ref_high']."</td><td>
                          <button type='button' name='enter' id='enter' data-toggle='modal' data-target='#add_data_Modal' class='enter btn btn-warning'>Edit</button><button class='btn btn btn-info btn-copy'>Copy</button></td><td class='ref_low_m'>"
                          .$row['ref_low_m']."</td><td>"
                          ."<button class='btn btn btn-info btn-copy'>Copy</button></td><td class='ref_high_m' >"
-                         .$row['ref_high_m']."</td><td>"
-                         .$row['critical_low']."</td><td>"
+                         .$row['ref_high_m']."</td><td class='critical_low'>"
+                         .$row['critical_low']."</td><td class='critical_high'>"
                          .$row['critical_high']."</td><td>"
                          ."<button class='btn btn btn-info btn-copy'>Copy</button></td><td class='critical_low_m' >"
                          .$row['critical_low_m']."</td><td>"
